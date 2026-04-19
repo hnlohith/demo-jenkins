80813df6459bec0d53bca667f09445348b13855f
hnlohith <hnlohithshivamogga@gmail.com>
2026-04-19 17:33:45 +0530

    Add build checking message to Hellojenkins

--- Hellojenkins.java
+++ Hellojenkins.java
@@ -1,5 +1,6 @@
 public class Hellojenkins {
     public static void main(String[] args) {
         System.out.println("Hello, jenkinsss!");
+        System.out.println("this is for build checking!!");
     }
 }Jenkins test
