a985c844d15b917d870f4974938f557c460141de
hnlohith <hnlohithshivamogga@gmail.com>
2026-04-19 17:38:34 +0530

    just checking msg for build test in jenkins ci

--- Hellojenkins.java
+++ Hellojenkins.java
@@ -2,5 +2,6 @@ public class Hellojenkins {
     public static void main(String[] args) {
         System.out.println("Hello, jenkinsss!");
         System.out.println("this is for build checking!!");
+        System.out.println("this is for build checking 2222!!");        
     }
 }Jenkins test
