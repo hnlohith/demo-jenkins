04983fa6ab2af34a7aebe6a1f761a2256e371c2b
hnlohith <hnlohithshivamogga@gmail.com>
2026-04-24 08:37:44 +0530

    Clean up Hellojenkins.java from merge conflicts
    Removed merge conflict markers and test lines.

--- Hellojenkins.java
+++ Hellojenkins.java
@@ -4,10 +4,4 @@ public class Hellojenkins {
         System.out.println("this is for build checking!!");
         System.out.println("this is for build checking 2222!!");        
     }
-<<<<<<< HEAD
-}Jenkins test
-test
-//test change
-=======
 }
->>>>>>> 9352a38ff18c7a5d0cf92aec5212eb359d9753df
