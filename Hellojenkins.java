aa02ccce284ff1bbb590e19fd8035057730a1dbf
Lohith H N <hnlohithshivamogga@gmail.com>
2026-04-24 08:42:15 +0530

    test to tirgger

--- Hellojenkins.java
+++ Hellojenkins.java
@@ -2,12 +2,8 @@ public class Hellojenkins {
     public static void main(String[] args) {
         System.out.println("Hello, jenkinsss!");
         System.out.println("this is for build checking!!");
-        System.out.println("this is for build checking 2222!!");        
+        System.out.println("this is for build checking 2222!!");      
+        //test  
     }
-<<<<<<< HEAD
-}Jenkins test
-test
-//test change
-=======
+
 }
->>>>>>> 9352a38ff18c7a5d0cf92aec5212eb359d9753df
