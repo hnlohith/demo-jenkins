93d317c5a3d25ce939b17a2addecc0414d0d67ca
Lohith H N <hnlohithshivamogga@gmail.com>
2026-05-08 15:37:44 +0530

    Testing auto trigger

--- Hellojenkins.java
+++ Hellojenkins.java
@@ -2,7 +2,8 @@ public class Hellojenkins {
     public static void main(String[] args) {
         System.out.println("Hello, jenkinsss!");
         System.out.println("this is for build checking!!");
-        System.out.println("this is for build checking 2222!!");      
+        System.out.println("this is for build checking 2222!!");   
+        System.out.println("Auto Jenkins Trigger Working");   
         //test  
     }
 }
